cc6cc44a0e42c34a28ef231ce28888de2d3db40c
Martucia <imarta.shlapak@gmail.com>
2024-01-18 18:51:32 +0200

    init

---
+++ src/entities/tasks/queries.ts
@@ -0,0 +1,74 @@
+import {
+  TaskDto,
+  tasksControllerCreate,
+  tasksControllerGetAll,
+  tasksControllerPatch,
+  tasksControllerRemove,
+} from "@/shared/api/generated";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const queryKey = ["tasks"];
+
+export function useTaskListQuery(categoryId: number | undefined) {
+  return useQuery({
+    queryKey: [`${queryKey[0]}_${categoryId}`],
+    queryFn: () => tasksControllerGetAll(Number(categoryId)),
+    enabled: categoryId !== undefined,
+    staleTime: Infinity,
+    placeholderData: [],
+    retry: false
+    
+  });
+}
+
+export function useAddTaskMutation() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: tasksControllerCreate,
+    async onSettled(data) {
+      if (data) {
+        await queryClient.invalidateQueries({
+          queryKey: [`tasks_${data.ownerId}`],
+        });
+      }
+    },
+  });
+}
+
+export function useRemoveTaskMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: tasksControllerRemove,
+    async onSettled(data) {
+      if (data) {
+        await queryClient.invalidateQueries({
+          queryKey: [`tasks_${data.ownerId}`],
+        });
+      }
+    },
+  });
+}
+
+export function usePatchTaskMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: tasksControllerPatch,
+    onSuccess(data) {
+      queryClient.setQueryData([`tasks_${data.ownerId}`], (prevData: any) => {
+        const updatedData = prevData.map((task: TaskDto) =>
+          task.id === data.id ? data : task,
+        );
+        return updatedData;
+      });
+    },
+    async onSettled(data) {
+      if (data) {
+        await queryClient.invalidateQueries({
+          queryKey: [`tasks_${data.ownerId}`],
+        });
+      }
+    },
+  });
+}
